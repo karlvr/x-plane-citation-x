dadb077d4d04812c89b2eebb99cb132d4e12d129
Karl von Randow <karl@xk72.com>
2023-09-25 12:36:35 +1300

    Tidies after X-Plane update

--- plugins/xlua/scripts/CitX_electrical/CitX_electrical.lua
+++ plugins/xlua/scripts/CitX_electrical/CitX_electrical.lua
@@ -126,14 +126,12 @@ end
 function cmd_battery_left_on(phase, duration)
 	if phase == 0 then
 		battery_left_value = 1
-		--evaluate_stby_factor()
 	end
 end
 
 function cmd_battery_left_off(phase, duration)
 	if phase == 0 then
 		battery_left_value = 0
-		--evaluate_stby_factor()
 	end
 end
 
@@ -148,14 +146,12 @@ end
 function cmd_battery_right_on(phase, duration)
 	if phase == 0 then
 		battery_right_value = 1
-		--evaluate_stby_factor()
 	end
 end
 
 function cmd_battery_right_off(phase, duration)
 	if phase == 0 then
 		battery_right_value = 0
-		--evaluate_stby_factor()
 	end
 end
 
@@ -271,15 +267,7 @@ cmdgenerLdwn = create_command("laminar/CitX/electrical/cmd_generator_left_dwn","
 cmdgenerRup = create_command("laminar/CitX/electrical/cmd_generator_right_up","Generator R switch up",cmd_generator_right_up)
 cmdgenerRdwn = create_command("laminar/CitX/electrical/cmd_generator_right_dwn","Generator R switch down",cmd_generator_right_dwn)
 cmdbatteryLtog = create_command("laminar/CitX/electrical/cmd_battery_left_toggle","Battery L switch toggle",cmd_battery_left_toggle)
-cmdbatteryLon = create_command("laminar/CitX/electrical/cmd_battery_left_on","Battery L switch on",cmd_battery_left_on)
-cmdbatteryLoff = create_command("laminar/CitX/electrical/cmd_battery_left_off","Battery L switch off",cmd_battery_left_off)
-replace_command("sim/electrical/battery_1_on",cmd_battery_left_on)
-replace_command("sim/electrical/battery_1_off",cmd_battery_left_off)
 cmdbatteryRtog = create_command("laminar/CitX/electrical/cmd_battery_right_toggle","Battery R switch toggle",cmd_battery_right_toggle)
-cmdbatteryRon = create_command("laminar/CitX/electrical/cmd_battery_right_on","Battery R switch on",cmd_battery_right_on)
-cmdbatteryRoff = create_command("laminar/CitX/electrical/cmd_battery_right_off","Battery R switch off",cmd_battery_right_off)
-replace_command("sim/electrical/battery_2_on",cmd_battery_right_on)
-replace_command("sim/electrical/battery_2_off",cmd_battery_right_off)
 cmdextpwrtog = create_command("laminar/CitX/electrical/cmd_ext_pwr_toggle","External Power switch toggle",cmd_ext_pwr_toggle)
 cmdstbypwrup = create_command("laminar/CitX/electrical/cmd_stby_pwr_up","Standby power switch up",cmd_stby_pwr_up)
 cmdstbypwrdwn = create_command("laminar/CitX/electrical/cmd_stby_pwr_dwn","Standby power switch down",cmd_stby_pwr_dwn)
@@ -295,6 +283,16 @@ replace_command("sim/systems/gnd_com_power_on",cmd_avionics_eicas_on)
 replace_command("sim/systems/gnd_com_power_off",cmd_avionics_eicas_off)
 
 
+-- Extra commands for simpits (XPD-13818), not used here.
+create_command("laminar/CitX/electrical/cmd_battery_left_on","Battery L switch on", cmd_battery_left_on)
+create_command("laminar/CitX/electrical/cmd_battery_left_off","Battery L switch off", cmd_battery_left_off)
+create_command("laminar/CitX/electrical/cmd_battery_right_on","Battery R switch on", cmd_battery_right_on)
+create_command("laminar/CitX/electrical/cmd_battery_right_off","Battery R switch off", cmd_battery_right_off)
+
+replace_command("sim/electrical/battery_1_on",cmd_battery_left_on)
+replace_command("sim/electrical/battery_1_off",cmd_battery_left_off)
+replace_command("sim/electrical/battery_2_on",cmd_battery_right_on)
+replace_command("sim/electrical/battery_2_off",cmd_battery_right_off)
 
 --------------------------------- DO THIS EACH FLIGHT START ---------------------------------
 function flight_start()
